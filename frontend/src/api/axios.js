26e4b7f2edd374e717e68763760973600690dc46
Harsh kumar <hkumar51622@gmail.com>
2026-06-13 12:54:52 +0530

    Update axios.js

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://127.0.0.1:8000/api/",
+  baseURL: "https://ai-resume-interview-production.up.railway.app/",
 });
 
 // Access token automatically attach
