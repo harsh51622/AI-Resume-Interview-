5db9f26efe09967bde7afaee2916c206a0f5fa7a
Harsh kumar <hkumar51622@gmail.com>
2026-06-13 11:56:28 +0530

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -76,10 +76,10 @@ import os
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.environ.get("MYSQLDATABASE"),
-        'USER': os.environ.get("MYSQLUSER"),
-        'PASSWORD': os.environ.get("MYSQLPASSWORD"),
-        'HOST': os.environ.get("MYSQLHOST"),
+        'NAME': os.environ.get("MYSQLDATABASE", ""),
+        'USER': os.environ.get("MYSQLUSER", ""),
+        'PASSWORD': os.environ.get("MYSQLPASSWORD", ""),
+        'HOST': os.environ.get("MYSQLHOST", ""),
         'PORT': os.environ.get("MYSQLPORT", "3306"),
     }
 }
