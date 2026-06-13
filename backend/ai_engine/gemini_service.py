e55e38adb5ef10ac282ee71956cf05731d538da5
Harsh kumar <hkumar51622@gmail.com>
2026-06-13 12:31:32 +0530

    Update gemini_service.py

--- backend/ai_engine/gemini_service.py
+++ backend/ai_engine/gemini_service.py
@@ -2,22 +2,27 @@ import os
 from dotenv import load_dotenv
 from .prompts import QUESTION_GENERATION_PROMPT, INTERVIEW_EVALUATION_PROMPT
 from google import genai
+
 load_dotenv()
-
-
 api_key = os.getenv("API_KEY")
-
-genai.configure(api_key=api_key)
-model = genai.GenerativeModel("gemini-2.5-flash")
+client = genai.Client(api_key=api_key)
 
 
 def generate_questions(resume_text):
     prompt = QUESTION_GENERATION_PROMPT.format(resume_text=resume_text)
-    response= model.generate_content(prompt)
+    response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents=prompt
+    )
+
     return response.text
+
 
 def evaluate_interview(interview_data):
     prompt = INTERVIEW_EVALUATION_PROMPT.format(interview_data=interview_data)
-    response = model.generate_content(prompt)
-    return response.text
+    response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents=prompt
+    )
 
+    return response.text
